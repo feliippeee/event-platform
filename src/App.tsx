d9103d41bc7f8776bbe52d97f7677e54cf6bd0cc
feliippeee <felipe.tecnosuporte@gmail.com>
2022-06-21 22:09:48 -0300

    aula 1

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+export default function App() {
+
+  const GET_LESSONS_QUERY = gql`
+    query {
+      lessons {
+        id 
+        title
+      }
+    }
+  `
+  const { data } = useQuery<{lessons : Lesson[]}>(GET_LESSONS_QUERY)
+
+  console.log(data)
+  
+  return (
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
+}
+
