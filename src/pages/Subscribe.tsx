7169e3d2c797e7694ea54168c1cc67ebb433a1e2
feliippeee <felipe.tecnosuporte@gmail.com>
2022-06-26 00:53:20 -0300

    add import img

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import imgUrl from '../../src/assets/code-mockup.png';
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
@@ -65,7 +66,7 @@ export function Subscribe() {
                     </form>
                 </div>
             </div>
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={imgUrl} className="mt-10" alt="" />
         </div> 
     )
 }
